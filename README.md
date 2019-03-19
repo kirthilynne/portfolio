8186cbf6a9eb21d590991e5bb398681e3abb97e5
Kirthi Lynne <kirthilynne@icloud.com>
2019-03-19 13:37:47 -0400

    updates

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Kirthi Lynne : Web Developer 
 
+## Technologies Used
+* Animate.css
+* Jquery
+* Typewriter.js
+
+## Concepts Utilized
+* flexbox
+* event listeners
+* media queries
+* css transitions
+* 
--- assets/code.js
+++ assets/code.js
@@ -1,6 +1,36 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    console.log('hello world')
-  });
+document.addEventListener("DOMContentLoaded", function (event) {
 
-const about = document.getElementById('about');
+  // var counter = 0;
+  // const subtitle = $('.subtitle');
+  // setInterval(() => {
+  //   if (counter > words.length - 1) {
+  //     counter = 0;
+  //   }
+  //   const text = words[counter];
+  //   subtitle.text(text);
+  //   counter++;
+  // }, 1000);
+
+  var app = document.getElementById('typewriter');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('I am: ')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    // .pauseFor(2500)
+    .start();
+
+
+});
+
+const words = ['Web Developer', 'Photographer', 'JavaScript Enthusiest', 'Logic Lover', 'Animal Fanatic']
+
+
 
